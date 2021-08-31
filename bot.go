50f01e3af4136cf66c13e079ef6d88c58ac8da96
mohammadg <gcoder.dev@gmail.com>
2021-08-31 11:52:50 +0430

    modified bot

--- bot.go
+++ bot.go
@@ -53,17 +53,19 @@ func (u User) SendMessageToUser(b Bot, text string) {
 	if u.Id == 0 {
 		log.Fatalln("User's Id field is empty")
 	}
-	req, _ := http.NewRequest("GET", fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", b.Token), nil)
+	req, err := http.NewRequest("GET", fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", b.Token), nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
 	q := req.URL.Query()
 	q.Add("chat_id", strconv.Itoa(u.Id))
 	q.Add("text", text)
 	req.URL.RawQuery = q.Encode()
 	client := &http.Client{}
-	_, err := client.Do(req)
+	_, err = client.Do(req)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	client.CloseIdleConnections()
 }
 
 type Message struct {
