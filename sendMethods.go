67b3fe5bdb122d3461987111fb7afd7feb7bbcf7
mohammadg <gcoder.dev@gmail.com>
2021-09-04 12:04:23 +0430

    modified SendPhotoLogic

--- sendMethods.go
+++ sendMethods.go
@@ -4,9 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"net/url"
 	"strconv"
-	"strings"
 )
 
 func sendTextLogic(b Bot, id int, text string) {
@@ -50,35 +48,35 @@ func (c Chat) SendText(b Bot, text string) {
 }
 
 func sendPhotoLogic(b Bot, id int, photo string) {
-	//req, err := http.NewRequest("GET", fmt.Sprintf("https://api.telegram.org/bot%s/sendPhoto", b.Token), nil)
-	//if err != nil {
-	//	log.Fatalln(err)
-	//}
-	//q := req.URL.Query()
-	//q.Add("chat_id", strconv.Itoa(id))
-	//q.Add("photo", photo)
-	//req.URL.RawQuery = q.Encode()
-	//client := &http.Client{}
-	//_, err = client.Do(req)
-	//if err != nil {
-	//	log.Fatalln(err)
-	//}
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("https://api.telegram.org/bot%s/sendPhoto", b.Token), nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	q := req.URL.Query()
+	q.Add("chat_id", strconv.Itoa(id))
+	q.Add("photo", photo)
+	req.URL.RawQuery = q.Encode()
+	client := &http.Client{}
+	_, err = client.Do(req)
+	if err != nil {
+		log.Fatalln(err)
+	}
 	//body := &bytes.Buffer{}
 	//writer := multipart.NewWriter(body)
 	//part, err := writer.CreateFormFile(filetype, filepath.Base(file.Name()))
 	//io.Copy(part, file)
 	//writer.Close()
-	data := url.Values{}
-	data.Set("chat_id", strconv.Itoa(id))
-	data.Set("photo", photo)
-	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("https://api.telegram.org/bot%s/sendPhoto", b.Token),
-		strings.NewReader(data.Encode()))
-	client := &http.Client{}
-	res, err := client.Do(req)
-	if err != nil {
-		log.Println(err)
-	}
-	log.Println(res)
+	//data := url.Values{}
+	//data.Set("chat_id", strconv.Itoa(id))
+	//data.Set("photo", photo)
+	//req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("https://api.telegram.org/bot%s/sendPhoto", b.Token),
+	//	strings.NewReader(data.Encode()))
+	//client := &http.Client{}
+	//res, err := client.Do(req)
+	//if err != nil {
+	//	log.Println(err)
+	//}
+	//log.Println(res)
 }
 
 func (u User) SendPhoto(b Bot, photo string) {
