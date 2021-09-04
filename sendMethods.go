2781e29124a422c11f2e0aa44776bd5d656db1cb
mohammadg <gcoder.dev@gmail.com>
2021-09-04 13:25:15 +0430

    modified SendPhotoLogic

--- sendMethods.go
+++ sendMethods.go
@@ -10,7 +10,7 @@ import (
 )
 
 func sendTextLogic(b Bot, id int, text string) {
-	req, err := http.NewRequest("GET", fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", b.Token), nil)
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", b.Token), nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -53,11 +53,10 @@ func sendPhotoLogic(b Bot, id int, photo string) {
 	q := url.Values{}
 	q.Add("chat_id", strconv.Itoa(id))
 	q.Add("photo", photo)
-	//req.URL.RawQuery = q.Encode()
 	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("https://api.telegram.org/bot%s/sendPhoto", b.Token),
 		strings.NewReader(q.Encode()))
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
 	}
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 	//q := req.URL.Query()
@@ -68,22 +67,6 @@ func sendPhotoLogic(b Bot, id int, photo string) {
 		log.Fatalln(err)
 	}
 	log.Println(res)
-	//body := &bytes.Buffer{}
-	//writer := multipart.NewWriter(body)
-	//part, err := writer.CreateFormFile(filetype, filepath.Base(file.Name()))
-	//io.Copy(part, file)
-	//writer.Close()
-	//data := url.Values{}
-	//data.Set("chat_id", strconv.Itoa(id))
-	//data.Set("photo", photo)
-	//req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("https://api.telegram.org/bot%s/sendPhoto", b.Token),
-	//	strings.NewReader(data.Encode()))
-	//client := &http.Client{}
-	//res, err := client.Do(req)
-	//if err != nil {
-	//	log.Println(err)
-	//}
-	//log.Println(res)
 }
 
 func (u User) SendPhoto(b Bot, photo string) {
