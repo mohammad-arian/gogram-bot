cb4e24140dcd8b7a108331c54e511eae483f7dc3
mohammadg <gcoder.dev@gmail.com>
2021-09-18 11:31:15 +0430

    added RemoveReplyKeyboard method

--- configs.go
+++ configs.go
@@ -89,6 +89,17 @@ func (t *TextOptionalParams) AddReplyKeyboardButtonRow(i ...KeyboardButton) {
 	}
 }
 
+// RemoveReplyKeyboard Removes the reply keyboard.
+// Set selective to true if you want to remove the keyboard for specific users only.
+// Targets: 1) users that are @mentioned in the text of the Message object;
+//          2) if the bot's message is a reply (has reply_to_message_id), sender of the original message.
+// Example: A user votes in a poll, bot returns confirmation message in reply
+// to the vote and removes the keyboard for that user,
+// while still showing the keyboard with poll options to users who haven't voted yet.
+func (t *TextOptionalParams) RemoveReplyKeyboard(selective bool) {
+	t.ReplyMarkup = &ReplyKeyboardRemove{RemoveKeyboard: true, Selective: selective}
+}
+
 type PhotoOptionalParams struct {
 	ParseMode                string          `json:"parse_mode"`
 	Caption                  string          `json:"caption"`
