155a336e4b98af331773ef60081f6f1e269b86c7
gcoder-dev <gcoder.dev@gmail.com>
2022-02-02 12:00:03 +0330

    improved RemoveReplyKeyboard

--- types.go
+++ types.go
@@ -692,7 +692,7 @@ type ReplyKeyboardRemove struct {
 	Selective      bool `json:"selective"`
 }
 
-// Remove removes the reply keyboard.
+// Remove sets ReplyKeyboardRemove fields.
 // Set selective to true if you want to remove the keyboard for specific users only.
 // Targets: 1) users that are @mentioned in the text of the Message object;
 //          2) if the bot's message is a reply (has reply_to_message_id), sender of the original message.
@@ -717,7 +717,7 @@ func (t *ForceReply) SetForceReply(selective bool, inputFieldPlaceholder string)
 }
 
 type Keyboard struct {
-	ReplyMarkup interface{} `json:"reply_markup"`
+	ReplyMarkup interface{} `json:"reply_markup,omitempty"`
 }
 
 func (k *Keyboard) SetInlineKeyboard(horizontal bool, a ...InlineButton) error {
@@ -756,6 +756,13 @@ func (k *Keyboard) SetReplyKeyboard(optionalParams AddReplyKeyboardData, a ...Re
 	return nil
 }
 
+// RemoveReplyKeyboard removes reply keyboard.
+// Set selective to true if you want to remove the keyboard for specific users only.
+// Targets: 1) users that are @mentioned in the text of the Message object;
+//          2) if the bot's message is a reply (has reply_to_message_id), sender of the original message.
+// Example: A user votes in a poll, bot returns confirmation message in reply
+// to the vote and removes the keyboard for that user,
+// while still showing the keyboard with poll options to users who haven't voted yet.
 func (k *Keyboard) RemoveReplyKeyboard(selective bool) {
 	i := ReplyKeyboardRemove{}
 	i.Remove(selective)
